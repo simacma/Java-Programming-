7a2785d376f955da0833012a0ac029a56949ff10
Yogesh132 <50366051+Yogesh132@users.noreply.github.com>
2022-04-26 20:44:11 +0545

    Adding practice quesation to github

---
+++ JavaLearning/src/com/coreJava/JavaPracticeQuestion/JavaQuestion5.java
@@ -0,0 +1,2 @@
+package com.coreJava.JavaPracticeQuestion;public class JavaQuestion5 {
+}
---
+++ JavaLearning/src/com/coreJava/JavaPracticeQuestion/javaQuestion6.java
@@ -0,0 +1,11 @@
+//WAP to find even series sum up to n=50.
+package com.coreJava.JavaPracticeQuestion;
+
+public class javaQuestion6 {
+    public static void main(String[] args) {
+        int n=50;
+        int sum=0;
+        sum=n*(n+1);
+        System.out.println("The sum of first n even number ="+ sum);
+    }
+}
